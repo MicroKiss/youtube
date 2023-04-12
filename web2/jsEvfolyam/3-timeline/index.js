2463bf291b972ef545a0d380da1ae7be58a296a6
Micro <dinnyetok@gmail.com>
2023-04-12 17:58:40 +0200

    ELTE IK web2 js evfolyam 3-4

--- web2/jsEvfolyam/3-timeline/index.js
+++ web2/jsEvfolyam/3-timeline/index.js
@@ -8,3 +8,84 @@ let pixelsPerYear = 5;
 let origoYear = 1000;
 let dt = 0.2;
 
+function GetX(year) {
+	return (year - origoYear) * pixelsPerYear;
+}
+
+
+function ReDraw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//a
+	ctx.strokeStyle = 'red';
+	ctx.fillStyle = 'black';
+	for (let y = 1000; y <= 1500; y += 50) {
+		ctx.beginPath();
+		ctx.moveTo(GetX(y), 0);
+		ctx.lineTo(GetX(y), canvas.height);
+		ctx.stroke();
+		ctx.fillText(y, GetX(y), 10);
+	}
+
+	//b
+
+
+	ctx.strokeStyle = 'black';
+	for (const king of arpads) {
+		ctx.fillStyle = 'green';
+		ctx.beginPath();
+		let width = (GetX(king["to"]) - GetX(king["from"])) * pixelsPerYear;
+		ctx.rect(GetX(king["from"]), 60, width, 60);
+		ctx.fill();
+		ctx.stroke();
+		ctx.fillStyle = 'black';
+		ctx.fillText(king["name"], GetX(king["from"]) + 2, 70);
+		ctx.fillText(king["from"] + '-' + king["to"], GetX(king["from"]) + 2, 80);
+	}
+
+
+	//cd
+
+	for (const king of plantanegets) {
+		ctx.fillStyle = 'red';
+		ctx.strokeStyle = 'black';
+		ctx.beginPath();
+		let width = (GetX(king["to"]) - GetX(king["from"])) * pixelsPerYear;
+		ctx.rect(GetX(king["from"]), 150, width, 60);
+		ctx.fill();
+		ctx.stroke();
+		ctx.fillStyle = 'black';
+		ctx.fillText(king["name"], GetX(king["from"]) + 2, 160);
+		ctx.fillText(king["from"] + '-' + king["to"], GetX(king["from"]) + 2, 170);
+	}
+}
+
+ReDraw();
+//e
+leftButton.onclick = () => {
+	origoYear -= 10;
+	ReDraw();
+}
+
+//f
+rightButton.onclick = () => {
+	origoYear += 10;
+	ReDraw();
+}
+
+//g
+
+var animID = undefined;
+function Anim() {
+	origoYear += dt;
+	ReDraw();
+	animID = window.requestAnimationFrame(Anim);
+}
+
+animButton.onclick = () => {
+	if (animID) {
+		cancelAnimationFrame(animID);
+		animID = undefined;
+	} else {
+		Anim();
+	}
+}
--- web2/jsEvfolyam/4-attendance-list/index.js
+++ web2/jsEvfolyam/4-attendance-list/index.js
@@ -17,3 +17,72 @@ function delegate(parent, type, selector, handler) {
   });
 }
 
+//a
+function SetPresents() {
+  const cells = table.getElementsByTagName('td');
+
+  for (const cell of cells) {
+    let checkBox = cell.getElementsByTagName('input')[0];
+    if (!checkBox)
+      continue;
+    if (checkBox.checked)
+      cell.classList.add('present')
+    else
+      cell.classList.remove('present')
+  }
+}
+
+SetPresents();
+
+//b
+
+delegate(table, 'click', 'td', (event) => {
+  let checkBox = event.target.getElementsByTagName('input')[0];
+  if (checkBox) {
+    checkBox.focus();
+  }
+})
+
+//c
+delegate(table, 'click', 'input', (event) => {
+  SetPresents();
+})
+
+//def
+document.addEventListener('keydown', (event) => {
+  if (document.activeElement.tagName !== 'INPUT')
+    return;
+  let newCoords = xyCoord(document.activeElement.parentElement);
+  let x = newCoords.x;
+  let y = newCoords.y;
+  switch (event.key) {
+    case 'ArrowRight':
+      newCoords.x++;
+      break;
+    case 'ArrowLeft':
+      newCoords.x--;
+      break;
+    case 'ArrowUp':
+      newCoords.y--;
+      break;
+    case 'ArrowDown':
+      newCoords.y++;
+      break;
+    default:
+      return;
+  }
+  console.log(x, y);
+  if (newCoords.x < 1)
+    newCoords.x = 7
+  else if (newCoords.x > 7)
+    newCoords.x = 1
+
+  if (newCoords.y < 0)
+    newCoords.y = 2
+  else if (newCoords.y > 2)
+    newCoords.y = 0
+
+  let td = table.rows[newCoords.y + 1].cells[newCoords.x];
+  let checkBox = td.getElementsByTagName('input')[0];
+  checkBox.focus();
+})
